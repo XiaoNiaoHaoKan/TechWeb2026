f00f433e253ea911f40c3f49523b6532c4233d9e
Cane sotto neve <yy@bologna.italia.it>
2026-08-28 19:04:32 +0200

    minor bug fix

--- server_marketplace/app.js
+++ server_marketplace/app.js
@@ -17,7 +17,11 @@ function createApp() {
   app.use(express.static(ROOT_DIR));
 
   app.use((req, res) => {
-    res.status(404).sendFile(path.join(ROOT_DIR, 'marketplace.html'));
+    if (req.path.startsWith('/api/')) {
+      return res.status(404).json({ error: 'Endpoint API non trovato.' });
+    }
+
+    return res.sendFile(path.join(ROOT_DIR, 'marketplace.html'));
   });
 
   return app;
